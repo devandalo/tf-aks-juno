80e0288ec23948ae0654437a46d7ad516d08ac1f
Asaad <aeacoder@gmail.com>
2024-05-15 21:18:10 +0200

    temp remove ref to prometheus

--- mw-dce.tf
+++ mw-dce.tf
@@ -1,6 +1,6 @@
-resource "azurerm_monitor_data_collection_endpoint" "aks" {
-  name                = format("dce-%s", var.stack)
-  resource_group_name = module.rg.name
-  location            = module.rg.location
-  kind                = "Linux"
-}
+# resource "azurerm_monitor_data_collection_endpoint" "aks" {
+#   name                = format("dce-%s", var.stack)
+#   resource_group_name = module.rg.name
+#   location            = module.rg.location
+#   kind                = "Linux"
+# }
